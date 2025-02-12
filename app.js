b1bca3ab4afc6661cb53ecb4ebb6ccb6cfd79f91
Sara <saracantu2009@hotmail.com>
2025-02-11 22:12:43 -0600

    Se programó el botón de sortear amigo secreto

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ function agregarNombres() {
     if (numeroDeNombres == numeroMaximo) {
         alert ("Ya llego al número maximo de nombres")
     }
-    else {
-    let nombreInsertado = document.getElementById("amigo").value;
-    if (nombreInsertado ==="") {
-        alert ("Porfavor, inserte un nombre");
-    }
-    else {
-        listaNombres.push(nombreInsertado);
-        document.getElementById("amigo").value="";
-        console.log(listaNombres)
-        listaDeAmigos()
-        numeroDeNombres = numeroDeNombres + 1
+        else {
+        let nombreInsertado = document.getElementById("amigo").value;
+            if (nombreInsertado ==="") {
+            alert ("Porfavor, inserte un nombre");
+            }
+                else {
+                    listaNombres.push(nombreInsertado);
+                    document.getElementById("amigo").value="";
+                    console.log(listaNombres)
+                    listaDeAmigos()
+                    numeroDeNombres = numeroDeNombres + 1
     }
   }
     return;
@@ -25,9 +25,9 @@ function agregarNombres() {
 function listaDeAmigos() {
     lista=[];
     console.log("se ejecutó listaDeAmigos");
-    for (let i = 0; i < listaNombres.length; i++) {
-      lista+= `<li>${listaNombres[i]}</li>`;
-      actualizarListaAmigos()
+        for (let i = 0; i < listaNombres.length; i++) {
+            lista+= `<li>${listaNombres[i]}</li>`;
+            actualizarListaAmigos()
     }
     return lista;
   }
@@ -39,4 +39,21 @@ function actualizarListaAmigos() {
   
     let resultadoList = document.getElementById("resultado");
     resultadoList.innerHTML = `Has agregado ${listaNombres.length} amigos.`;
+    return
 }
+
+function sortearAmigo() {
+    if (listaNombres == [])
+    alert ("No hay nombres, favor de escribirlos");
+    else {
+        let numeroSorteado = parseInt(Math.floor(Math.random()*listaNombres.length));
+        let nombreSorteado = listaNombres[numeroSorteado];
+        console.log(nombreSorteado);
+
+        lista = `<li>${nombreSorteado[numeroSorteado]}</li>`
+        let listaConAmigos = document.getElementById("listaAmigos");
+        listaConAmigos.innerHTML = lista;
+        let resultadoList = document.getElementById("resultado");
+    resultadoList.innerHTML = `¡Felicidades ese es tu nombre sorteado!`;
+    }
+    }
