1a13a390de986ff4f8934785400df81793aa865a
Sara <saracantu2009@hotmail.com>
2025-02-11 21:47:24 -0600

    Se terminó de programar la lista de amigos, falta el sorteo

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 let listaNombres = [];
 let numeroMaximo = 5;
+let numeroDeNombres = 0;
 
 function agregarNombres() {
-    let nombreInsertado = document.getElementById("amigo");
-    if (nombreInsertado ==="string") {
+    console.log("se ejecuta agregar Nombres")
+    if (numeroDeNombres == numeroMaximo) {
+        alert ("Ya llego al número maximo de nombres")
+    }
+    else {
+    let nombreInsertado = document.getElementById("amigo").value;
+    if (nombreInsertado ==="") {
         alert ("Porfavor, inserte un nombre");
     }
     else {
         listaNombres.push(nombreInsertado);
-        document.getElementById("amigo")=" ";
+        document.getElementById("amigo").value="";
+        console.log(listaNombres)
+        listaDeAmigos()
+        numeroDeNombres = numeroDeNombres + 1
     }
+  }
+    return;
+}
+function listaDeAmigos() {
+    lista=[];
+    console.log("se ejecutó listaDeAmigos");
+    for (let i = 0; i < listaNombres.length; i++) {
+      lista+= `<li>${listaNombres[i]}</li>`;
+      actualizarListaAmigos()
+    }
+    return lista;
+  }
+  
+function actualizarListaAmigos() {
+    console.log("Se ejecuto actualizarListaAmigos")
+    let listaConAmigos = document.getElementById("listaAmigos");
+    listaConAmigos.innerHTML = lista;
+  
+    let resultadoList = document.getElementById("resultado");
+    resultadoList.innerHTML = `Has agregado ${listaNombres.length} amigos.`;
 }
