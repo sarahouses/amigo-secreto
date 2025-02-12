f01831a81bc7b7d32b3e95b3c8ff693a6dbdd5a4
Sara <saracantu2009@hotmail.com>
2025-02-11 19:54:15 -0600

    Editar function guardarNombres

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ let numeroMaximo = 5;
 
 function guardarNombres() {
     let nombreInsertado = document.getElementById("amigo");
+    if (nombreInsertado =="") {
+        alert ("Porfavor, inserte un nombre");
+    }
+    else {
+        listaNombres.push(nombreInsertado);
+        document.getElementById("amigo")=" ";
+    }
 }
